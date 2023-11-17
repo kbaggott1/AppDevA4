523655aa9a212309b140cdf50f1a029ff0c07802
kbaggott1 <kevin.baggott1@gmail.com>
2023-11-17 16:13:33 -0500

    Create DataStore

--- codelab-android-datastore-master/app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
+++ codelab-android-datastore-master/app/src/main/java/com/codelab/android/datastore/data/UserPreferencesRepository.kt
@@ -18,6 +18,8 @@ package com.codelab.android.datastore.data
 
 import android.content.Context
 import androidx.core.content.edit
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -36,7 +38,7 @@ data class UserPreferences(val showCompleted: Boolean)
 /**
  * Class that handles saving and retrieving user preferences
  */
-class UserPreferencesRepository private constructor(context: Context) {
+class UserPreferencesRepository constructor(private val dataStore: DataStore<Preferences>, context: Context) {
 
     private val sharedPreferences =
         context.applicationContext.getSharedPreferences(USER_PREFERENCES_NAME, Context.MODE_PRIVATE)
@@ -97,23 +99,6 @@ class UserPreferencesRepository private constructor(context: Context) {
     private fun updateSortOrder(sortOrder: SortOrder) {
         sharedPreferences.edit {
             putString(SORT_ORDER_KEY, sortOrder.name)
-        }
-    }
-
-    companion object {
-        @Volatile
-        private var INSTANCE: UserPreferencesRepository? = null
-
-        fun getInstance(context: Context): UserPreferencesRepository {
-            return INSTANCE ?: synchronized(this) {
-                INSTANCE?.let {
-                    return it
-                }
-
-                val instance = UserPreferencesRepository(context)
-                INSTANCE = instance
-                instance
-            }
         }
     }
 }
