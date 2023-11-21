bc03de40de12668e62141c0ec77cd12e381f5580
kbaggott1 <kevin.baggott1@gmail.com>
2023-11-21 11:09:22 -0500

    Modify code to use async()

--- codelab-basic-coroutines/app/src/main/java/com/example/basic_coroutines/MainActivity.kt
+++ codelab-basic-coroutines/app/src/main/java/com/example/basic_coroutines/MainActivity.kt
@@ -33,28 +33,26 @@ class MainActivity : ComponentActivity() {
 }
 
 fun Main() {
-    val time = measureTimeMillis {
-        runBlocking {
-            println("Weather forecast")
-            launch {
-                printForecast()
-            }
-
-            launch {
-                printTemperature()
-            }
-            println("Have a good day!")
+    runBlocking {
+        println("Weather forecast")
+        val forecast: Deferred<String> = async {
+            getForecast()
         }
+        val temperature: Deferred<String> = async {
+            getTemperature()
+        }
+
+        println("${forecast.await()} ${temperature.await()}")
+        println("Have a good day!")
     }
-    println("Execution time: ${time / 1000.0} seconds")
 }
 
-suspend fun printForecast() {
+suspend fun getForecast(): String {
     delay(1000)
-    println("Sunny")
+    return "Sunny"
 }
 
-suspend fun printTemperature() {
+suspend fun getTemperature(): String  {
     delay(1000)
-    println("30\u00b0C")
+    return "30\u00b0C"
 }
