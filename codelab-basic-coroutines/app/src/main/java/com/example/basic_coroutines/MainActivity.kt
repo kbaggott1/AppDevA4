6243f27921e595c7c60f367900f77c629a824768
kbaggott1 <kevin.baggott1@gmail.com>
2023-11-21 11:12:06 -0500

    Refactor to use parallel decomposition of work

--- codelab-basic-coroutines/app/src/main/java/com/example/basic_coroutines/MainActivity.kt
+++ codelab-basic-coroutines/app/src/main/java/com/example/basic_coroutines/MainActivity.kt
@@ -35,14 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Main() {
     runBlocking {
         println("Weather forecast")
-        val forecast: Deferred<String> = async {
-            getForecast()
-        }
-        val temperature: Deferred<String> = async {
-            getTemperature()
-        }
-
-        println("${forecast.await()} ${temperature.await()}")
+        println(getWeatherReport())
         println("Have a good day!")
     }
 }
@@ -55,4 +48,10 @@ suspend fun getForecast(): String {
 suspend fun getTemperature(): String  {
     delay(1000)
     return "30\u00b0C"
+}
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async { getTemperature() }
+    "${forecast.await()} ${temperature.await()}"
 }
