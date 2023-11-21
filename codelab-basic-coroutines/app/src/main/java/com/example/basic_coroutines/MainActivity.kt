028c3e63726abb704518f6ecb08a8129989b870d
kbaggott1 <kevin.baggott1@gmail.com>
2023-11-21 11:22:40 -0500

    Use worker thread to run code and keep the main thread unblocked

--- codelab-basic-coroutines/app/src/main/java/com/example/basic_coroutines/MainActivity.kt
+++ codelab-basic-coroutines/app/src/main/java/com/example/basic_coroutines/MainActivity.kt
@@ -34,28 +34,16 @@ class MainActivity : ComponentActivity() {
 
 fun Main() {
     runBlocking {
-        println("Weather forecast")
-        println(getWeatherReport())
-        println("Have a good day!")
+        println("${Thread.currentThread().name} - runBlocking function")
+        launch {
+            println("${Thread.currentThread().name} - launch function")
+            withContext(Dispatchers.Default) {
+                println("${Thread.currentThread().name} - withContext function")
+                delay(1000)
+                println("10 results found.")
+            }
+            println("${Thread.currentThread().name} - end of launch function")
+        }
+        println("Loading...")
     }
-}
-
-suspend fun getForecast(): String {
-    delay(1000)
-    return "Sunny"
-}
-
-suspend fun getTemperature(): String  {
-    delay(1000)
-    return "30\u00b0C"
-}
-
-suspend fun getWeatherReport() = coroutineScope {
-    val forecast = async { getForecast() }
-    val temperature = async { getTemperature() }
-
-    delay(200)
-    temperature.cancel()
-
-    "${forecast.await()}"
 }
